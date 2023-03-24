aa9e4b6711f4e38acf0cfb4ca182ee268d0fd032
Jason Zondor <jason@zondor.com>
2023-03-24 13:30:29 -0500

    Update CU_GS_config.js

--- 2023/CU_GS_config.js
+++ 2023/CU_GS_config.js
@@ -17,7 +17,7 @@ var config_data = `
       "code": "e",
       "gsCol": "event",
       "type": "event",
-      "defaultValue": "2023mokc",
+      "defaultValue": "2023mose",
       "required": "true",
       "disabled": "true"
     },
