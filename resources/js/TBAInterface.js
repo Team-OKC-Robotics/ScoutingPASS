365b34e0029f5c037e31ee18e5918a555b5fa97e
Jason Zondor <jason@zondor.com>
2023-03-23 19:04:22 -0500

    Update TBAInterface.js
    Adding our TBA key

--- resources/js/TBAInterface.js
+++ resources/js/TBAInterface.js
@@ -1,7 +1,7 @@
 // TBAInterface funcitons to pull data from TheBlueAlliance.com
 var teams = null;
 var schedule = null;
-var authKey = "uTHeEfPigDp9huQCpLNkWK7FBQIb01Qrzvt4MAjh9z2WQDkrsvNE77ch6bOPvPb6";
+var authKey = "a8pwDlgKPaiJ4K2HMksYWk3hZVIKIDJV9YVtUbRNjtGWrJtHAXF7k12B5bv8dlzx";
 /**
  * Get list of teams in event
  *
