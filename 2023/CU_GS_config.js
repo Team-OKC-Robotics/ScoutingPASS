35fa9f0711ad9d3e87626844c662bbb877483080
Jason Zondor <jason@zondor.com>
2023-04-20 07:32:40 -0500

    Update CU_GS_config.js
    Updating to Worlds Hopper Division

--- 2023/CU_GS_config.js
+++ 2023/CU_GS_config.js
@@ -17,7 +17,7 @@ var config_data = `
       "code": "e",
       "gsCol": "event",
       "type": "event",
-      "defaultValue": "2023mokc",
+      "defaultValue": "2023hop",
       "required": "true",
       "disabled": "true"
     },
