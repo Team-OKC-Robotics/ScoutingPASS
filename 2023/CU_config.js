8cbb348f707896ccbd7895f6437b99816d8387c7
Jason Zondor <jason@zondor.com>
2023-04-20 07:35:07 -0500

    Update CU_config.js

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -14,7 +14,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2023mokc",
+      "defaultValue": "2023hop",
       "required": "true",
       "disabled": "true"
     },
